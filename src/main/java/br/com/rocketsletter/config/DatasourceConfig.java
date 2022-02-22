17da13a802b95808221c0a31c9f163f1b2eea80b
Jonatas Freitas <jonatasfreitas20@gmail.com>
2022-02-22 08:06:02 -0300

    Testing heroku automatic deploy.

--- src/main/java/br/com/rocketsletter/config/DatasourceConfig.java
+++ src/main/java/br/com/rocketsletter/config/DatasourceConfig.java
@@ -1,12 +1,9 @@
 package br.com.rocketsletter.config;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
